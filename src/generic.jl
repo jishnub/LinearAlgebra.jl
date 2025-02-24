bf21c74f99c5fca47b477b419e13359472826fb5
Jishnu Bhattacharya <jishnub.github@gmail.com>
2025-02-24 15:50:38 +0530

    Use row and col supports in copytrito!

--- src/generic.jl
+++ src/generic.jl
@@ -2138,14 +2138,14 @@ function copytrito!(B::AbstractMatrix, A::AbstractMatrix, uplo::AbstractChar)
         LAPACK.lacpy_size_check(size(B), (n < m ? n : m, n))
         # extract the parents for UpperTriangular matrices
         Bv, Av = uppertridata(B), uppertridata(A)
-        for j in axes(A,2), i in axes(A,1)[begin : min(j,end)]
+        for j in rowsupport(A), i in intersect(colsupport(A,j), axes(A,1)[begin : min(j,end)])
             @inbounds Bv[i,j] = Av[i,j]
         end
     else # uplo == 'L'
         LAPACK.lacpy_size_check(size(B), (m, m < n ? m : n))
         # extract the parents for LowerTriangular matrices
         Bv, Av = lowertridata(B), lowertridata(A)
-        for j in axes(A,2), i in axes(A,1)[j:end]
+        for j in rowsupport(A), i in intersect(colsupport(A,j), axes(A,1)[j:end])
             @inbounds Bv[i,j] = Av[i,j]
         end
     end
